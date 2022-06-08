dd17f748be2b42a0ecd749116e557742242aeaef
cse18107 <cse18107@cemk.ac.in>
2022-06-08 22:19:01 +0530

    worked on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,40 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import search from "../../images/search.png";
 import logo from "../../images/bookmyshow.jpg";
 
-
 const Header = () => {
-
-    const [show, setShow] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
   return (
-
     <div className="header-body">
       <div className="header-container center">
         <div className="header-content">
           <div className="header-content_left">
             <div className="header-content_left--logo">
-              <img src={logo} alt='bookmyshow'/>
+              <img src={logo} alt="bookmyshow" />
             </div>
             <div className="header-content_left--searchbar">
               <div className="search_icon">
                 <img src={search} alt="search" />
               </div>
               <div className="search_field">
-                  <input placeholder="Search for Movies, Events, Plays, Sports and Activities"/>
+                <input placeholder="Search for Movies, Events, Plays, Sports and Activities" />
               </div>
             </div>
           </div>
           <div className="header-content_right center">
             <div className="header-content_right--auth">
-                <button>Sign In</button>
+              <button onClick={() => setShowOverlay(true)}>Sign In</button>
             </div>
-            <div className="header-content_right--burgermenu" onClick={()=>setShow(!show)}>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div
+              className="header-content_right--burgermenu"
+              onClick={() => setShowSidebar(!showSidebar)}
+            >
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </div>
@@ -42,9 +43,27 @@ const Header = () => {
         <div className="header-panel_left"></div>
         <div className="header-panel_right"></div>
       </div>
-      <div className="overlay" style={{display:`${show?'block':'none'}`}} onClick={()=>setShow(false)}></div>
-      <div className="sidebar" style={{right:`${show?'0px':'-350px'}`}}>
-
+      <div
+        className="overlay"
+        style={{ display: `${showSidebar ? "block" : "none"}` }}
+        onClick={() => setShowSidebar(false)}
+      ></div>
+      <div
+        className="sidebar"
+        style={{ right: `${showSidebar ? "0px" : "-350px"}` }}
+      >
+        {/* code */}
+      </div>
+      <div className="backdrop"  style={{ display: `${showOverlay ? "block" : "none"}` }}></div>
+      <div className={`model ${
+              showOverlay ? "model-open" : "model-close"
+            }`}>
+            <p
+              style={{ cursor: "pointer" }}
+              onClick={() => setShowOverlay(false)}
+            >
+              close
+            </p>
       </div>
     </div>
   );
